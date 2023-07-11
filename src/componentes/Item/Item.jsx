8e2b4c7d6576c1b82b1486cd82447b98520e30e0
natalia <oggiani.nat@outlook.com>
2023-07-10 22:03:45 -0300

    ajuste

--- src/componentes/Item/Item.jsx
+++ src/componentes/Item/Item.jsx
@@ -6,7 +6,7 @@ const Item = ({id, nombre, precio, img}) => {
     <div className='card'>
         <img className='imgCard' src={img} alt={nombre}/>
         <h3>{nombre}</h3>
-        <p>Precio: {precio}</p>
+        <p>$ {precio}</p>
         <p>ID:{id}</p>
         <Link to={`/item/${id}`} > Ver Detalles </Link>
     </div>
